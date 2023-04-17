752b475df63c05f7fb8da1650274aace20c26be5
ciscon <ciscon@nicotinelounge.com>
2023-04-17 14:25:11 -0400

    term

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font:pixelsize=12:antialias=true:autohint=true:rgba=none:hintstyle=hintfull";
+static char *font = "Hack Nerd Font:pixelsize=11:antialias=true:autohint=true:rgba=none:hintstyle=hintfull";
 
 static int borderpx = 2;
 
