bd773d392166e37ca2b933528f91f9346bd66937
ciscon <ciscon@nicotinelounge.com>
2023-08-02 16:46:01 -0400

    use system default mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=12:antialias=true:autohint=true:rgba=none:hintstyle=hintfull";
+static char *font = "Mono:pixelsize=12:antialias=true:autohint=true:rgba=none:hintstyle=hintfull";
 
 static int borderpx = 2;
 
